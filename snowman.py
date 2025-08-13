2cb3fef65748acd4c7078f1a8241668b3778573e
elektrox <elektrox@gmail.com>
2025-08-13 12:44:09 +0000

    play again option

--- snowman.py
+++ snowman.py
@@ -1,4 +1,11 @@
 from game_logic import play_game
 
 if __name__ == "__main__":
-    play_game()
+    while True:
+        play_game()
+        
+        # Ask user if they want to play again
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again not in ["yes", "y"]:
+            print("Thanks for playing!")
+            break
